0caa346e356c61cc2fee5c7243416323403d9eb5
Jamuna <jamunaadhikari112@gmail.com>
2025-09-24 21:48:26 -0700

    first commit

---
+++ src/main/java/aws/cloud_basics/CloudBasicsApplication.java
@@ -0,0 +1,13 @@
+package aws.cloud_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aws/cloud_basics/CloudBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.cloud_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
