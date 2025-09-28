4e128cb58d95042b19c71a2199f46f2372139110
Jamuna <jamunaadhikari112@gmail.com>
2025-09-28 14:41:09 -0700

    upload/download file from s3

--- src/main/java/aws/cloud_basics/config/S3Config.java
+++ src/main/java/aws/cloud_basics/config/S3Config.java
@@ -1,9 +1,12 @@
 package aws.cloud_basics.config;
 
-import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
@@ -18,6 +21,15 @@ public class S3Config {
     private String region;
 
 
+    @Bean
+    public S3Client s3Client() {
+         AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+            return   S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(basicCredentials))
+                .build();
+
+    }
 
 
 }
